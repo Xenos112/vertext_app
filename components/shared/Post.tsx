154ca5f75ef415a67f62731d7961f53811c0773a
Xenos112 <acristyano22@gmail.com>
2025-02-01 01:48:01 -0800

    fix indent in the post shared component

--- components/shared/Post.tsx
+++ components/shared/Post.tsx
@@ -1,4 +1,5 @@
 import { type APIResponse } from "@/types/api";
+import { isImage } from '@/constants/index'
 import { FaRegComment, FaRegHeart, FaHeart } from "react-icons/fa";
 import { GET } from "@/app/api/feed/route";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -160,12 +161,12 @@ export default function Post({ ...post }: PostProps) {
     postState.medias.length === 1
       ? "grid-cols-1 grid-rows-1"
       : postState.medias.length === 2
-      ? "grid-cols-2 grid-rows-1"
-      : postState.medias.length === 3
-      ? "grid-cols-3 grid-rows-1"
-      : postState.medias.length === 4
-      ? "grid-cols-4 grid-rows-1"
-      : "grid-cols-5 grid-rows-1";
+        ? "grid-cols-2 grid-rows-1"
+        : postState.medias.length === 3
+          ? "grid-cols-3 grid-rows-1"
+          : postState.medias.length === 4
+            ? "grid-cols-4 grid-rows-1"
+            : "grid-cols-5 grid-rows-1";
 
   return (
     <div className="py-3 border-gray-500 border-b">
@@ -303,14 +304,18 @@ export default function Post({ ...post }: PostProps) {
           />
         )}
         {postState.medias.length !== 0 && (
-          <div className={`${className} grid gap-1 mt-3`}>
+          <div className={`${className} rounded-md overflow-hidden grid gap-1 mt-3`}>
             {postState.medias.map((media, index) => (
               <AspectRatio key={index} ratio={16 / 9}>
-                <img
-                  src={media}
-                  alt="Post"
-                  className="object-cover size-full"
-                />
+                {isImage.test(media) ? (
+                  <img
+                    src={media}
+                    alt="Post"
+                    className="object-cover size-full"
+                  />)
+                  : (
+                    <video src={media} />
+                  )}
               </AspectRatio>
             ))}
           </div>
