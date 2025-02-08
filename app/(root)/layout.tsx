ebb75479d05e92e4e5cce8ae9e58738dd0545b52
Xenos112 <acristyano22@gmail.com>
2025-02-08 13:36:11 +0000

    add a button in the root layout to open a modal that the user can create posts from

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,24 @@
 import LeftBar from "@/components/layout/LeftBar";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import CreatePostModal from "@/features/post/components/CreatePostModal";
 import { type ReactNode } from "react";
+import { GoPlus } from "react-icons/go";
 
 export default function layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen">
       <LeftBar />
       <div className="w-[550px] mx-auto">{children}</div>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <button className="p-4 bg-primary text-primary-foreground fixed bottom-4 right-4 rounded-xl">
+            <GoPlus size={24} />
+          </button>
+        </DialogTrigger>
+        <CreatePostModal />
+      </Dialog>
     </div>
   );
 }
