8639ad6e748acfe2828eacd3915d431db2313f82
Xenos112 <acristyano22@gmail.com>
2025-02-11 13:50:51 +0000

    remove nested message in the errors object

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 export const ERRORS = {
-  NOT_AUTHENTICATED: { message: "you must be logged in" },
-  POST_NOT_FOUND: { message: "Post Not Found" },
+  NOT_AUTHENTICATED: "you must be logged in",
+  POST_NOT_FOUND: "Post Not Found",
 };
 
 export const isImage = /\.*(.png|.jpg|.jpeg|.webp)/i;
