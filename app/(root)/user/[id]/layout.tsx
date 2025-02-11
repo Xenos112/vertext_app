294c38bd65833f094e4f5df871e04eff24558970
Xenos112 <acristyano22@gmail.com>
2025-02-11 14:00:16 +0000

    make the ring color same as the background

--- app/(root)/user/[id]/layout.tsx
+++ app/(root)/user/[id]/layout.tsx
@@ -107,7 +107,7 @@ export default function UserPage({ children }: { children: ReactNode }) {
               src={user?.banner_url!}
               className="h-[200px] w-full object-cover"
             />
-            <Avatar className="size-[130px] absolute ring-4 ring-offset-transparent ring-white -translate-y-1/2 mx-3">
+            <Avatar className="size-[130px] absolute ring-4 ring-offset-transparent ring-background -translate-y-1/2 mx-3">
               <AvatarImage src={user?.image_url!} />
               <AvatarFallback>
                 {formatUserNameForImage(user?.user_name!)}
