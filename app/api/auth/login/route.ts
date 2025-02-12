d3d2615791f3aa935d5de488b6dfc8b47b3901d0
Xenos112 <acristyano22@gmail.com>
2025-02-12 20:45:01 +0000

    trim the user input in the server side

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -11,10 +11,12 @@ import { STATUS_CODES } from "@/constants";
 const LOGIN_VALIDATOR = z.object({
   email: z
     .string({ message: "Email is Not Provided" })
-    .email({ message: "Email Schema is Not Valid" }),
+    .email({ message: "Email Schema is Not Valid" })
+    .trim(),
   password: z
     .string({ message: "Password is Not Provided" })
-    .min(8, { message: "Passowrd Length is lesst then 8" }),
+    .min(8, { message: "Passowrd Length is lesst then 8" })
+    .trim(),
 });
 
 export const POST = async (req: NextRequest) => {
--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -12,13 +12,16 @@ const REGISTER_VALIDATOR = z.object({
   user_name: z
     .string({ message: "Username is Not Provided" })
     .min(5, { message: "Username Must be less then 5 chars" })
-    .max(20, { message: "Username Must not be more then 20 chars" }),
+    .max(20, { message: "Username Must not be more then 20 chars" })
+    .trim(),
   email: z
     .string({ message: "Email is Not Provided" })
-    .email({ message: "Email Schema is Not Valid" }),
+    .email({ message: "Email Schema is Not Valid" })
+    .trim(),
   password: z
     .string({ message: "Password is Not Provided" })
-    .min(8, { message: "Passowrd Length is lesst then 8" }),
+    .min(8, { message: "Passowrd Length is lesst then 8" })
+    .trim(),
 });
 
 export const POST = async (req: NextRequest) => {
