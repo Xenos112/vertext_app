bbbfd3a924eee95429c43f475075b0fa53f9bdeb
Xenos112 <acristyano22@gmail.com>
2025-02-13 21:43:27 +0000

    feat:add forget passwod confirmation api route

--- app/api/auth/forget-password/confirmation/route.ts
+++ app/api/auth/forget-password/confirmation/route.ts
@@ -1,1 +1,50 @@
-export const POST = async () => {};
+import { STATUS_CODES } from "@/constants";
+import { FORGET_PASSWORD_CONFIRM_VALIDATOR } from "@/features/auth/validators/forget-password";
+import bcrypt from "bcrypt";
+import formatZodErrors from "@/utils/format-zod-errors";
+import prisma from "@/utils/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const requestJson = await req.json();
+
+    const { error, data, success } =
+      FORGET_PASSWORD_CONFIRM_VALIDATOR.safeParse(requestJson);
+    if (!success) {
+      const errors = formatZodErrors(error);
+      return NextResponse.json(
+        { errors },
+        { status: STATUS_CODES.BAD_REQUEST },
+      );
+    }
+
+    const resetPasswordRequest = await prisma.resetPassword.findUnique({
+      where: { id: data.id },
+      include: {
+        User: true,
+      },
+    });
+
+    if (!resetPasswordRequest) {
+      return NextResponse.json(
+        { error: "Can't Find your Reset Password request" },
+        { status: STATUS_CODES.UNAUTHORIZED },
+      );
+    }
+
+    const passwordHash = bcrypt.hashSync(data.password, 10);
+    await prisma.user.update({
+      where: { id: resetPasswordRequest.User.id },
+      data: {
+        password: passwordHash,
+      },
+    });
+  } catch (error) {
+    console.log("ERROR: " + error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: STATUS_CODES.SERVER_ISSUE },
+    );
+  }
+};
--- features/auth/validators/forget-password.ts
+++ features/auth/validators/forget-password.ts
@@ -5,3 +5,12 @@ export const FORGET_PASSWORD_VALIDATOR = z.object({
     .string({ message: "Please Provide an Email" })
     .email({ message: "This is Not a Valid Email" }),
 });
+
+export const FORGET_PASSWORD_CONFIRM_VALIDATOR = z.object({
+  id: z
+    .string({ message: "Id id Not Provided" })
+    .uuid({ message: "This is Not a Valid Id" }),
+  password: z
+    .string()
+    .min(8, { message: "Password Length is less then 8 chars" }),
+});
