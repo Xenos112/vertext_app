87c503c657737e5bca809cc4e9d45b9302fcaf23
Xenos112 <acristyano22@gmail.com>
2025-03-10 17:47:50 +0000

    feat: add lib and api functions to help fetch the data

---
+++ features/chat/api/queryMessages.ts
@@ -0,0 +1,15 @@
+import ky from "ky";
+import type { GetCommunityMessagesResponse } from "@/api";
+
+export default async function queryMessages(communityId: string) {
+  const res = await ky.get<GetCommunityMessagesResponse>("/api/chat/get-community-messages", {
+    searchParams: {
+      communityId
+    },
+    throwHttpErrors: false
+  });
+  const data = await res.json()
+
+  if ("error" in data) throw new Error(data.error);
+  return data
+}
---
+++ features/chat/lib/getMessages.ts
@@ -0,0 +1,26 @@
+import prisma from "@/utils/prisma";
+
+/**
+ * a function to get messages from a community
+ * @param communityId - the id of the community to get messages from
+ * @returns - an array of messages from the community
+ * @description a function to get messages from a community
+*/
+export default async function getMessages(communityId: string) {
+  const messages = await prisma.message.findMany({
+    where: {
+      communityId
+    },
+    include: {
+      Sender: {
+        omit: {
+          password: true
+        }
+      },
+    },
+    take: 20,
+  })
+
+  return messages
+}
+
