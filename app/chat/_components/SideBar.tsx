32170a73a3e747b507e99454d497190e4c636c76
Xenos112 <acristyano22@gmail.com>
2025-03-12 15:47:00 +0000

    fix: make the icon go to the home page

--- app/chat/_components/SideBar.tsx
+++ app/chat/_components/SideBar.tsx
@@ -1,13 +1,14 @@
 import { SiNeovim } from "react-icons/si";
 import CommunityPreview from "./CommunitiesPreview";
 import { IoTelescopeOutline } from "react-icons/io5";
+import Link from "next/link";
 
 export default function SideBar() {
   return (
     <div className="sticky p-3 border-r overflow-scroll border-muted flex flex-col gap-4 items-center top-0 z-10">
-      <div>
+      <Link href="/">
         <SiNeovim size={30} />
-      </div>
+      </Link>
       <div>
         <CommunityPreview />
       </div>
