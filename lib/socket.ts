80ac706c7b1595871c1a4aa93abf118ced77a9a4
Xenos112 <acristyano22@gmail.com>
2025-03-14 22:05:13 +0000

    chore: remove FIX comments

--- lib/socket.ts
+++ lib/socket.ts
@@ -2,10 +2,7 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import prisma from "@/utils/prisma";
 
-
-// FIX: define the port in the .env file
-// FIX: make it in /chat and add notification socket
-const PORT = process.env.SOCKET_PORT || 3001
+const PORT = process.env.SOCKET_PORT || 3001;
 
 const httpServer = createServer();
 
@@ -13,8 +10,8 @@ const io = new Server(httpServer, {
   cors: {
     // HACK: only allow port 3000
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
@@ -25,24 +22,35 @@ io.on("connection", (socket) => {
     console.log(`User joined room: ${room}`);
   });
 
-  socket.on("sendMessage", async ({ room, sender, content }: { room: string; sender: string; content: string; }) => {
-    try {
-      const message = await prisma.message.create({
-        data: {
-          content,
-          senderId: sender,
-          communityId: room,
-        },
-        include: {
-          Sender: true,
-        }
-      });
+  socket.on(
+    "sendMessage",
+    async ({
+      room,
+      sender,
+      content,
+    }: {
+      room: string;
+      sender: string;
+      content: string;
+    }) => {
+      try {
+        const message = await prisma.message.create({
+          data: {
+            content,
+            senderId: sender,
+            communityId: room,
+          },
+          include: {
+            Sender: true,
+          },
+        });
 
-      io.to(room).emit("receiveMessage", message);
-    } catch (error) {
-      console.error("Error creating message:", error);
-    }
-  });
+        io.to(room).emit("receiveMessage", message);
+      } catch (error) {
+        console.error("Error creating message:", error);
+      }
+    },
+  );
 
   socket.on("disconnect", () => {
     console.log("A user disconnected");
