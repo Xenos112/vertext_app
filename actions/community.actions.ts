da3107f28bd7b8914cbabc332c4598bf277e1bd9
Xenos112 <acristyano22@gmail.com>
2025-03-15 04:27:34 +0000

    refactor: remove old code

--- actions/community.actions.ts
+++ actions/community.actions.ts
@@ -1,6 +1,4 @@
 "use server";
-
-import { ERRORS } from "@/constants";
 import prisma from "@/utils/prisma";
 import validateUser from "@/utils/validate-user";
 import { cookies } from "next/headers";
@@ -69,78 +67,5 @@ export async function joinCommunity(communityId: string) {
     return { message: "You Have Joined The Community Succesfully" };
   } catch (error) {
     return { error };
-  }
-}
-
-export async function leaveCommunity(communityId: string) {
-  try {
-    const cookieStore = await cookies();
-    const token = cookieStore.get("auth_token")?.value;
-    const user = await validateUser(token);
-    if (!user) redirect("/login");
-    // check if he is joined
-    const isJoined = await prisma.membership.findFirst({
-      where: {
-        AND: [{ communityId }, { userId: user.id }],
-      },
-    });
-
-    if (!isJoined) {
-      return { message: "You Have Already Left" };
-    }
-
-    // remove the membership
-    await prisma.membership.deleteMany({
-      where: {
-        AND: [{ communityId }, { userId: user.id }],
-      },
-    });
-
-    return { message: "You have left the Community" };
-  } catch (error) {
-    return { error };
-  }
-}
-
-type CreateCommuntyProps = {
-  name: string;
-  desc: string | null;
-  image: string | null;
-  banner: string | null;
-};
-export async function createCommunity({
-  name,
-  desc,
-  image,
-  banner,
-}: CreateCommuntyProps) {
-  try {
-    const cookieStore = await cookies();
-    const token = cookieStore.get("auth_token")?.value;
-    const user = await validateUser(token);
-    if (!user) {
-      return { error: ERRORS.NOT_AUTHENTICATED };
-    }
-
-    const community = await prisma.community.create({
-      data: {
-        name,
-        image,
-        banner,
-        bio: desc || "",
-      },
-    });
-    await prisma.membership.create({
-      data: {
-        role: "ADMIN",
-        userId: user.id,
-        communityId: community.id,
-      },
-    });
-
-    return { community };
-  } catch (error) {
-    console.log(error);
-    return { error: "Something Went Wrong" };
   }
 }
