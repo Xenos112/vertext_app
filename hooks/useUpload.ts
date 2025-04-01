c8ce2fdfefadb4319f7f7102d430f2a8ea62690e
Xenos112 <acristyano22@gmail.com>
2025-04-01 14:34:28 +0000

    refactor: move useUploadFile to useUpload

---
+++ hooks/useUpload.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import ky from "ky";
+
+export function useUpload() {
+  const [url, setUrl] = useState("");
+
+  async function upload(file: File) {
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await ky.post<{ url: string }>(
+      "http://localhost:8080/upload",
+      {
+        body: formData,
+        throwHttpErrors: false,
+      },
+    );
+
+    const data = await response.json();
+    if (!data || !data.url) {
+      document.dispatchEvent(
+        new CustomEvent("toast", {
+          detail: {
+            variant: "destructive",
+            title: "Upload Error",
+            description: "Failed to upload file",
+          },
+        }),
+      );
+    }
+
+    return url;
+  }
+
+  return { url, upload, setUrl };
+}
