56cc27c32c7360fa18a76478dc493a8664e902ea
Xenos112 <acristyano22@gmail.com>
2025-04-05 22:59:17 +0000

    fix: prefix api responses with APIResponse<> generic

--- app/api/v2/likes/[postId]/route.ts
+++ app/api/v2/likes/[postId]/route.ts
@@ -1,12 +1,13 @@
 import LikeService from "@/db/services/server/like.service";
+import { APIResponse } from "@/types/api";
 
 const GET = LikeService.getPostLikes;
 const POST = LikeService.createPostLike;
 const DELETE = LikeService.deletePostLike;
 
-type GetPostLikesRequest = ReturnType<typeof GET>;
-type CreatePostLikeRequest = ReturnType<typeof POST>;
-type DeletePostLikeRequest = ReturnType<typeof DELETE>;
+type GetPostLikesRequest = APIResponse<ReturnType<typeof GET>>;
+type CreatePostLikeRequest = APIResponse<ReturnType<typeof POST>>;
+type DeletePostLikeRequest = APIResponse<ReturnType<typeof DELETE>>;
 
 export {
   GET,
--- app/api/v2/posts/[id]/route.ts
+++ app/api/v2/posts/[id]/route.ts
@@ -1,12 +1,13 @@
 import PostService from "@/db/services/server/post.service";
+import { APIResponse } from "@/types/api";
 
 const GET = PostService.getPostById;
 const DELETE = PostService.deletePost;
 const PUT = PostService.updatePost;
 
-type GetPostByIdRequest = ReturnType<typeof GET>;
-type DeletePostByIdRequest = ReturnType<typeof DELETE>;
-type UpdatePostByIdRequest = ReturnType<typeof PUT>;
+type GetPostByIdRequest = APIResponse<ReturnType<typeof GET>>;
+type DeletePostByIdRequest = APIResponse<ReturnType<typeof DELETE>>;
+type UpdatePostByIdRequest = APIResponse<ReturnType<typeof PUT>>;
 
 export {
   GET,
