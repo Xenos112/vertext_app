1f7dda4ffaf8cdb63ecf6a40b97a58c3c504c9cd
Xenos112 <acristyano22@gmail.com>
2025-05-27 22:17:13 +0000

    fix: remove hard coded white color

--- components/shared/ReturnButton.tsx
+++ components/shared/ReturnButton.tsx
@@ -1,29 +1,35 @@
-"use client"
+"use client";
 
-import { GoArrowLeft } from "react-icons/go"
+import { GoArrowLeft } from "react-icons/go";
 
 type ReturnButtonProps = {
-  label: string
-  description?: string
-}
+  label: string;
+  description?: string;
+};
 
-export default function ReturnButton({ label, description }: ReturnButtonProps) {
+export default function ReturnButton({
+  label,
+  description,
+}: ReturnButtonProps) {
   const returnHandler = () => {
-    window.history.back()
-  }
+    window.history.back();
+  };
 
   return (
     <button
       onClick={returnHandler}
       className="group flex items-center gap-2.5 px-4 transition-all duration-200 rounded-lg"
     >
-      <GoArrowLeft size={20} className="text-gray-300 group-hover:text-white transition-colors" />
+      <GoArrowLeft
+        size={20}
+        className="text-gray-300 group-hover:text-white transition-colors"
+      />
       <div className="flex flex-col items-start">
-        <span className="text-white">{label}</span>
+        <span>{label}</span>
         {description && (
           <span className="text-muted-foreground text-xs">{description}</span>
         )}
       </div>
     </button>
-  )
+  );
 }
